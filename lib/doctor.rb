c581c9d112b5099cd8bf8a310cecc68fe2a79be7
sunny-tier-6236 <taycurrie@gmail.com>
2020-04-23 19:37:14 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -11,3 +11,16 @@ class Doctor
   def self.all
     @@all
   end
+
+  def new_appointment(date, patient)
+    Appointment.new(date, patient, self)
+  end
+
+  def appointments
+    Appointment.all.select { |appointment| appointment.doctor == self }
+  end
+
+  def patients
+    appointments.map(&:patient)
+  end
+end
